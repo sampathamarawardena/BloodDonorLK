2db695a255e20bc8838043b38ac77770a1ad91f7
ArunaAmarawardena <sampath.amarawardena@gmail.com>
2017-05-26 20:37:16 +0530

    Commite

---
+++ app/src/main/java/com/sasoftgroups/blooddonorlk/Contacts.java
@@ -0,0 +1,44 @@
+package com.sasoftgroups.blooddonorlk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Contacts extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Home:
+                Intent intent = new Intent(Contacts.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.donateList:
+                Intent intents = new Intent(Contacts.this, HomePage.class);
+                startActivity(intents);
+                return true;
+            case R.id.requestBlood:
+                Intent intentss = new Intent(Contacts.this, HomePage.class);
+                startActivity(intentss);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
--- app/src/main/java/com/sasoftgroups/blooddonorlk/MainActivity.java
+++ app/src/main/java/com/sasoftgroups/blooddonorlk/MainActivity.java
@@ -1,11 +1,44 @@
 package com.sasoftgroups.blooddonorlk;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Home:
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.donateList:
+                Intent intents = new Intent(MainActivity.this, HomePage.class);
+                startActivity(intents);
+                return true;
+            case R.id.requestBlood:
+                Intent intentss = new Intent(MainActivity.this, HomePage.class);
+                startActivity(intentss);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Click_MyProfile(View view) {
-        Intent intent = new Intent(MainActivity.this, HomePage.class);
+        Intent intent = new Intent(MainActivity.this, MyProfile.class);
         startActivity(intent);
     }
 
@@ -29,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Click_Contacts(View view) {
-        Intent intent = new Intent(MainActivity.this, HomePage.class);
+        Intent intent = new Intent(MainActivity.this, Contacts.class);
         startActivity(intent);
     }
 }

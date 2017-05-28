f6957f249af5b8f243f73919058e1f60131d157f
ArunaAmarawardena <sampath.amarawardena@gmail.com>
2017-05-28 11:26:34 +0530

    All Layouts

--- app/src/main/java/com/sasoftgroups/blooddonorlk/MyProfile.java
+++ app/src/main/java/com/sasoftgroups/blooddonorlk/MyProfile.java
@@ -10,33 +10,6 @@ import android.view.MenuItem;
 public class MyProfile extends AppCompatActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.Home:
-                Intent intent = new Intent(MyProfile.this, MainActivity.class);
-                startActivity(intent);
-                return true;
-            case R.id.donateList:
-                Intent intents = new Intent(MyProfile.this, HomePage.class);
-                startActivity(intents);
-                return true;
-            case R.id.requestBlood:
-                Intent intentss = new Intent(MyProfile.this, HomePage.class);
-                startActivity(intentss);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
